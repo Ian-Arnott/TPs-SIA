e8cf0242f7e5f0bc947641331ed728a7d654e91e
Julian Sasso <jsasso@itba.edu.ar>
2023-08-12 16:59:13 -0300

    First BFS implementation

--- Tp1/algorithms.py
+++ Tp1/algorithms.py
@@ -1,7 +1,31 @@
 from tree import Tree
+from collections import deque
+
+# https://docs.python.org/es/3/library/collections.html#collections.deque
+# collections.deque es una implementación alternativa de colas sin límites con operaciones atómicas rápidas
+# append() y popleft() que no requieren bloqueo y también soportan indexación.
 
 def bfs(gameState):
-    return 1,2,3
+    #return 1,2,3
+    exploredNodes = []
+    frontierNodes = deque()
+
+    tree = Tree(gameState)
+    root = tree.get_root()
+
+    frontierNodes.append(root)
+    while len(frontierNodes) > 0:
+        current = frontierNodes.popleft()
+
+        #TODO: Verificar si el nodo actual es la solución (Imprimirlo si lo es)
+
+        #TODO: A cuales puedo ir desde este state?
+        #TODO: Comparar como quedan los states de los nodos siguientes para no volver a recorrerlos
+        #TODO: Generar nodos para los states a los que puedo ir y agregarlos a frontierNodes
+
+        exploredNodes.append(current)
+
+
     
 def dfs(gameState):
     return 1,2,3
@@ -12,12 +36,6 @@ def greedy(gameState, heuristic):
 def astar(gameState, heuristic):
     return 1,2,3
 
-def general_solution():
-    exploredNodes = []
-    frontierNodes = []
-    #TODO:
-    initialState = None
-    tree = Tree(initialState)
-
+    
 
 
