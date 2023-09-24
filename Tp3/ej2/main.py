fa4d4f61209c34cdd75025f6dd49315324d44860
Julian Sasso <jsasso@itba.edu.ar>
2023-09-23 21:53:30 -0300

    Fixes

--- Tp3/ej2/main.py
+++ Tp3/ej2/main.py
@@ -2,6 +2,8 @@ import json
 from utils import get_config_params, get_data, PERCEPTRON_TYPES
 from perceptron import SimpleLinealPerceptron, SimpleHiperbolicPerceptron, SimpleLogisticPerceptrion
 
+
+
 def initialize_perceptron(perceptron_type, beta, learning_rate, bias, input_size):
     if perceptron_type == PERCEPTRON_TYPES[0]:
         return SimpleLinealPerceptron(learning_rate, None, bias, input_size)
@@ -10,6 +12,11 @@ def initialize_perceptron(perceptron_type, beta, learning_rate, bias, input_size
     elif perceptron_type == PERCEPTRON_TYPES[2]:
         return SimpleLogisticPerceptrion(beta, learning_rate, None, bias, input_size)
 
+
+def min_max_interval(values:list[float]) -> tuple[float]:
+    min_value = min(values)
+    max_value = max(values)
+    return (min_value, max_value)
 
 if __name__ == "__main__":
     with open('./config.json', 'r') as f:
@@ -28,7 +35,13 @@ if __name__ == "__main__":
 
     correct_predictions = 0 
 
-    result = perceptron.predict(testing_set)
+    if(perceptron_type == PERCEPTRON_TYPES[0]): # LINEAR
+        result = perceptron.predict(testing_set)
+    else:
+        # Escalo los resultados para que se ajusten al intervalo de entrenamiento
+        # TODO: Habria que hacerlo para [entrenamiento + testeo]?
+        result = perceptron.predict(testing_set, should_scale=True, scale_interval=min_max_interval(expected_data[:training_amount]))
+
     for i in range(len(result)):
         print(f'Result: {result[i]} - Expected: {expected_data[i+training_amount]}')
 
--- Tp3/ej2/perceptron.py
+++ Tp3/ej2/perceptron.py
@@ -17,7 +17,7 @@ class SimplePerceptron(ABC):
         self.learning_rate = learning_rate
         self.bias = bias
 
-
+ 
     @abstractmethod
     def activation_function(self, x:float) -> float:
         pass
@@ -27,33 +27,59 @@ class SimplePerceptron(ABC):
         pass
 
     def compute_error(self, input_data:list[list[float]], expected:list[float]):
-
         outputs:list[float] = self.current_outputs(input_data)
 
         total_error:float = 0
         for mu in range(len(expected)):
-            total_error += (expected[mu] - outputs[mu])**2
+            scaled_expected = self.activation_function(expected[mu])
+            total_error += (scaled_expected - outputs[mu])**2
+        #print(f"Current Error: {total_error/2}")
         return total_error/2
     
+    def theta(self, x:list[float]) -> float:
+        # Agrega un 1 al principio de x_mu para tener en cuenta el w_0
+        extended_x = np.array([1] + x)
+        return np.dot(extended_x, self.weights) + self.bias
+
 
     def current_outputs(self, input_data:list[list[float]]) -> list[float]:
-        return [self.activation_function(np.dot([1] + x, self.weights) + self.bias) for x in input_data]
+        return [self.activation_function(self.theta(x_mu)) for x_mu in input_data]
 
-    def delta_weights(self, excitement:float, activation:float, expected:float, x_mu:float):
-        return self.learning_rate * (expected - activation) * self.activation_derivative(excitement) * x_mu
+    def delta_weights(self, excitement:float, activation:float, expected:float, x_mu:list[float]):
+        extended_x_mu = np.array([1] + x_mu)
+        scaled_expected = self.activation_function(expected)
+        print("\tMissed by: ", (scaled_expected - activation))
+        print("\t\t", self.learning_rate)
+        print("\t\t", (scaled_expected - activation))
+        print("\t\t", self.activation_derivative(excitement))
+        print("\t\t", extended_x_mu)
+        print("\tDelta Weights: ", self.learning_rate * (scaled_expected - activation) * self.activation_derivative(excitement) * extended_x_mu)
+        return self.learning_rate * (scaled_expected - activation) * self.activation_derivative(excitement) * extended_x_mu
 
     def train(self, input_data:list[list[float]], expected:list[float], limit:int):
         current_steps = 0
         error_min = math.inf
         input_len = len(input_data)
 
+        print("Initial Weights: ", self.weights)
+
         while abs(error_min) > 0 and current_steps < limit:
+            print(f"======= Step: {current_steps} ==========")
             mu = random.randrange(0, input_len)
-            x_mu = np.array([1] + input_data[mu])
-            excitement = np.dot(x_mu, self.weights) + self.bias
+            print("Mu: ", mu, " -> ", input_data[mu])
+            x_mu = input_data[mu]
+            excitement = self.theta(x_mu)
             activation = self.activation_function(excitement)
 
-            self.weights += self.delta_weights(activation, excitement, expected[mu], x_mu)
+            # print("\n\n\nAdding weights: ")
+            # print(self.weights)
+            # print(" + ")
+            # print(self.delta_weights(excitement, activation, expected[mu], x_mu))
+            
+        
+            self.weights += self.delta_weights(excitement, activation, expected[mu], x_mu)
+            # print(" = ")
+            # print(self.weights)
 
             new_error = self.compute_error(input_data, expected)
             
@@ -61,14 +87,36 @@ class SimplePerceptron(ABC):
                 error_min = new_error
 
             current_steps += 1
+        
+        print(f"Steps: {current_steps} (limit: {limit})")
+        print(f"Min Error: {error_min}")
+        print("Final Weights: ", self.weights)
 
-    def predict_one(self, input:list[int]):
-        x_mu =  np.array([1] + input)
-        excitement = np.dot(x_mu, self.weights) + self.bias
-        return self.activation_function(excitement)
 
-    def predict(self, input_data:list[list[float]]) -> list[float]:
-        return [self.predict_one(x) for x in input_data]
+    def scale_result(self, value,  new_min, new_max):
+        scaled = ((value - self.activation_min) / (self.activation_max - self.activation_min)) * (new_max - new_min) + new_min
+        print("Scale: ", value , " -> ",scaled)
+        return scaled
+
+    def _predict_one(self, x:list[int], should_scale:bool, scale_interval:tuple[float]) -> float:
+        excitement = self.theta(x)
+        activation = self.activation_function(excitement)
+        print("Activation: ", activation)
+
+        if should_scale:
+            return self.scale_result(activation, scale_interval[0], scale_interval[1])
+        else:
+            return activation
+
+    def predict(self, input_data:list[list[float]], should_scale:bool = False, scale_interval:tuple[float] = ()) -> list[float]:
+        if should_scale:
+            if scale_interval is None or len(scale_interval) != 2:
+                raise Exception("Wrong scale_interval: size must be 2")
+            if scale_interval[0] > scale_interval[1]:
+                raise Exception("Wrong scale_interval: min > max")
+
+        return [self._predict_one(x, should_scale=should_scale, scale_interval=scale_interval) for x in input_data]
+
 
 
 
@@ -85,6 +133,8 @@ class SimpleHiperbolicPerceptron(SimplePerceptron):
 
     def __init__(self,  beta:float, learning_rate:float, weights, bias:float, input_data_dim:int,):
         self.beta = beta
+        self.activation_min = -1.0
+        self.activation_max = 1.0
         super().__init__(learning_rate, weights, bias, input_data_dim)
 
     # Imagen: (-1, 1)
@@ -99,8 +149,11 @@ class SimpleLogisticPerceptrion(SimplePerceptron):
 
     def __init__(self,  beta:float, learning_rate:float, weights, bias:float, input_data_dim:int,):
         self.beta = beta
+        self.activation_min = 0.0
+        self.activation_max = 1.0
         super().__init__(learning_rate, weights, bias, input_data_dim)
     
+    # Imagen: (0, 1)
     def activation_function(self, x:float) -> float:
         return 1 / (1 + math.exp(-self.beta * x))
     
