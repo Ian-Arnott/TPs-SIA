9c0714d0c30e997093f32409d8bd316615c0e262
Agustin Mattiussi <amattiussi@itba.edu.ar>
2023-09-26 20:43:36 -0300

    Removed unused files

--- Tp3/ej2/mse.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def mse(expected, predicted):
-    return np.mean(np.power(expected - predicted, 2))
-
-def mse_derivative(expected, predicted):
-    return 2 * (predicted - expected) / np.size(expected)
