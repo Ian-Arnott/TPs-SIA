d604470995fb33fc914ad91aaa3bf62cf8e7c4cc
Julian Sasso <jsasso@itba.edu.ar>
2023-10-13 16:19:36 -0300

    Implement winner neuron algorithm

--- Tp4/neuron.py
+++ Tp4/neuron.py
@@ -35,6 +35,27 @@ class KohonenNetwork:
         # Inicializa la red con k neuronas
         self.neuron_matrix = [[Neuron(self.get_sample(input_data), learning_rate) for _ in range(k)] for _ in range(k)]
 
+
     @staticmethod
     def get_sample(input_data: list[list[float]]) -> np.array[float]:
         return np.array(input_data[np.random.randint(len(input_data))])
+    
+
+    def process_input(self, input: np.array[float]):
+        # Calcula la similitud de cada neurona con la entrada
+        for i in range(self.k):
+            for j in range(self.k):
+                self.neuron_matrix[i][j].calculate_similitude(input)
+        
+        # Obtiene la neurona con menor similitud
+        winner_neuron = self._get_winner_neuron()
+        winner_neuron.groupCount += 1
+
+
+    def _get_winner_neuron(self) -> Neuron:
+        winner_neuron = self.neuron_matrix[0][0]
+        for i in range(self.k):
+            for j in range(self.k):
+                if self.neuron_matrix[i][j].activation < winner_neuron.activation:
+                    winner_neuron = self.neuron_matrix[i][j]
+        return winner_neuron
