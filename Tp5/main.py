ea16a7b061165a444a620d784902957667e19a6d
ianitba <iarnott@itba.edu.ar>
2023-11-12 12:14:21 -0300

    adds ej1 notebook

--- Tp5/main.py
+++ Tp5/main.py
@@ -6,13 +6,41 @@ from data.font import fontDict
 import numpy as np
 import matplotlib.pyplot as plt
 
+def visualize_results(original_data, reconstructed_data, num_samples=5):
+    fig, axes = plt.subplots(num_samples, 2, figsize=(8, 3 * num_samples))
+
+    for i in range(num_samples):
+        original_image = original_data[i].reshape((7, 5))
+        reconstructed_image = reconstructed_data[i][0].reshape((7, 5))
+
+        axes[i, 0].imshow(original_image, cmap='gray', aspect='auto')
+        axes[i, 0].set_title(f'Original - Sample {i + 1}')
+        axes[i, 0].axis('off')
+
+        axes[i, 1].imshow(reconstructed_image, cmap='gray', aspect='auto')
+        axes[i, 1].set_title(f'Reconstructed - Sample {i + 1}')
+        axes[i, 1].axis('off')
+
+    plt.suptitle('Autoencoder Results', fontsize=16)
+    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+    plt.show()
+
+def plot_latent_space(latent_space, labels):
+    latent_values = [point[1] for point in latent_space]
+    latent_values = np.array(latent_values)
+
+    plt.scatter(latent_values[:, 0], latent_values[:, 1], c=labels, cmap='viridis')
+    plt.title('Latent Space Visualization')
+    plt.xlabel('Latent Dimension 1')
+    plt.ylabel('Latent Dimension 2')
+    plt.colorbar()
+    plt.show()
+
 if __name__ == "__main__":
     bitmapDict = utils.fonts_to_bitmap(fontDict)
-
-    # Prepare data
     bitmapList = list(bitmapDict.values())  
     X = np.reshape(bitmapList,(len(bitmapList), 35, 1))
-    # Define Autoencoder Architecture
+
     autoencoder = [
         Dense(35, 10, optimizer_type="ADAM", learning_rate=0.001),
         Tanh(),
@@ -24,9 +52,5 @@ if __name__ == "__main__":
         Tanh(),
     ]
 
-    # Train Autoencoder
     train(autoencoder, mse, mse_derivative, X, X, epochs=1000, verbose=False)
-
-    # Evaluate the network by predicting on the training data
     reconstructed_data = [predict(autoencoder, x) for x in X]
-
